409dc21aaeff1cfb2f554b38791c1c6caf89873e
昊阳 <1467035545@qq.com>
2025-10-15 16:47:28 +0800

    混合Tvbox编辑器

--- src/config.ts
+++ src/config.ts
@@ -62,6 +62,11 @@ export const navBarConfig: NavBarConfig = {
 					url: "https://docs.qq.com/aio/DYmZYVGpFVGxOS3NE", // Internal links should not include the base path, as it is automatically added
 					external: true, // Show an external link icon and will open in a new tab
 				},
+				{
+					name: "TvBox编辑器",
+					url: "/tools/CatVodTVJsonEditor.html", // Internal links should not include the base path, as it is automatically added
+					external: false, // Show an external link icon and will open in a new tab
+				},
 			],
 		},
 	],
